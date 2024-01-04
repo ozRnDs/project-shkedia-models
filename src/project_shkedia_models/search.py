e1668733d2a850311a3b033f77da787fbb915b26
oz <oz.rnds@gmail.com>
2024-01-04 22:30:10 +0200

    feat(SearchResult): Add property number_of_pages

--- src/project_shkedia_models/search.py
+++ src/project_shkedia_models/search.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 from typing import List,Any
+from math import ceil
 
 class SearchResult(BaseModel):
     total_results_number: int
     page_number: int = 0
     page_size: int | None = None
     results: List[Any]
+
+    @property
+    def number_of_pages(self):
+        return ceil(self.total_results_number/self.page_size)
